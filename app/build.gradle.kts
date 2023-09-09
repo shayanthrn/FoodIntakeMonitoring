ae2445fe533a874b3d55218dad040cac5952b485
Hassan Kazemi Tehrani <shayanthrn@gmail.com>
2023-09-09 17:37:05 -0400

    add permission luncher, camera service luncher for preview, #todo start camera service in camera layout instead of main layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,11 @@ android {
 }
 
 dependencies {
-
+    //camerax Library
+    implementation("androidx.camera:camera-lifecycle:1.2.1")
+    implementation("androidx.camera:camera-camera2:1.2.1")
+    implementation("androidx.camera:camera-view:1.0.0-alpha19")
+    //camerax Library
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
--- app/src/main/java/ca/shayanthrn/foodintakemonitoring/MainActivity.kt
+++ app/src/main/java/ca/shayanthrn/foodintakemonitoring/MainActivity.kt
@@ -1,20 +1,83 @@
 package ca.shayanthrn.foodintakemonitoring
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.Manifest
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.core.*
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+
 
 
 class MainActivity : AppCompatActivity() {
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Log.i("Permission: ", "Granted")
+            } else {
+                Log.i("Permission: ", "Denied")
+            }
+        }
+    private val PERMISSION_REQUEST_CODE = 200
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Handler().postDelayed({
-                              startActivity(Intent(this,Camera::class.java))
-                              finish()
-        }, 5000)
+        requestPermissionLauncher.launch(
+            Manifest.permission.CAMERA
+        )
+        if(checkPermission()){
+            startCamera()
+            Handler().postDelayed({
+//                startActivity(Intent(this,Camera::class.java))
+//                finish()
+            }, 3000)
+        }else{
+            Toast.makeText(this, "This App requires your permission to work", Toast.LENGTH_SHORT).show()
+        }
+    }
 
+    private fun checkPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)==PackageManager.PERMISSION_GRANTED
+    }
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(findViewById<PreviewView>(R.id.viewFinder).surfaceProvider)
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview
+                )
+
+            } catch (exc: Exception) {
+                Log.e("TAG", "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
     }
 }
