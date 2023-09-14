746fee0f0dcd035bee2307b29df449ff282f2a78
Hassan Kazemi Tehrani <shayanthrn@gmail.com>
2023-09-13 23:27:33 -0400

    weight input activity added

---
+++ app/src/main/java/ca/shayanthrn/foodintakemonitoring/WeightInput.kt
@@ -0,0 +1,11 @@
+package ca.shayanthrn.foodintakemonitoring
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeightInput : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weight_input)
+    }
+}
