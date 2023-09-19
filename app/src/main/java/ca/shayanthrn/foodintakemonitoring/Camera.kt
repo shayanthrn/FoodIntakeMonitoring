e422c98ede9817390a87b4c5f39bdea9d2fe3dd1
Hassan Kazemi Tehrani <shayanthrn@gmail.com>
2023-09-19 11:29:04 -0400

    cleaning main activity code and camera activity code to get permission easier

--- app/src/main/java/ca/shayanthrn/foodintakemonitoring/Camera.kt
+++ app/src/main/java/ca/shayanthrn/foodintakemonitoring/Camera.kt
@@ -2,10 +2,48 @@ package ca.shayanthrn.foodintakemonitoring
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+
 
 class Camera : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+    }
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(findViewById<PreviewView>(R.id.viewFinder).surfaceProvider)
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview
+                )
+
+            } catch (exc: Exception) {
+                Log.e("TAG", "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
     }
 }
--- app/src/main/java/ca/shayanthrn/foodintakemonitoring/MainActivity.kt
+++ app/src/main/java/ca/shayanthrn/foodintakemonitoring/MainActivity.kt
@@ -1,19 +1,13 @@
 package ca.shayanthrn.foodintakemonitoring
 
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.Manifest
 import android.os.Bundle
-import android.os.Handler
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.camera.core.*
-import androidx.camera.view.PreviewView
-import androidx.core.content.ContextCompat
+
 
 
 
@@ -24,60 +18,18 @@ class MainActivity : AppCompatActivity() {
         ) { isGranted: Boolean ->
             if (isGranted) {
                 Log.i("Permission: ", "Granted")
+                startActivity(Intent(this,Camera::class.java))
+                finish()
             } else {
                 Log.i("Permission: ", "Denied")
+                Toast.makeText(this, "This App requires your permission to work", Toast.LENGTH_SHORT).show()
             }
         }
-    private val PERMISSION_REQUEST_CODE = 200
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         requestPermissionLauncher.launch(
             Manifest.permission.CAMERA
         )
-        if(checkPermission()){
-            startCamera()
-            Handler().postDelayed({
-//                startActivity(Intent(this,Camera::class.java))
-//                finish()
-            }, 3000)
-        }else{
-            Toast.makeText(this, "This App requires your permission to work", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun checkPermission(): Boolean {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)==PackageManager.PERMISSION_GRANTED
-    }
-    private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
-
-        cameraProviderFuture.addListener({
-            // Used to bind the lifecycle of cameras to the lifecycle owner
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            // Preview
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(findViewById<PreviewView>(R.id.viewFinder).surfaceProvider)
-                }
-
-            // Select back camera as a default
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
-                // Bind use cases to camera
-                cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview
-                )
-
-            } catch (exc: Exception) {
-                Log.e("TAG", "Use case binding failed", exc)
-            }
-
-        }, ContextCompat.getMainExecutor(this))
     }
 }
